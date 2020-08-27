62df5375b552a1e1120a3f5d2720dde592620cb0
Jon Decker <jondecker76@gmail.com>
2020-08-27 11:39:42 -0400

    Working on structuring the project

---
+++ example_callback.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+import PySimpleGUI as sg
+import PySimpleSQL as ss                               # <=== PySimpleSQL lines will be marked like this.  There's only a few!
+
+# Here are our callback functions
+def en(db,win):
+    res=sg.popup_get_text('Enter password for edit mode.\n(Hint: it is 1234)')
+    return True if res=='1234' else False
+def dis(db,win):
+    res = sg.popup_yes_no('Are you sure you want to disabled edit mode?')
+    return True if res == 'Yes' else False
+
+# Define our layout. We will use the ss.record convenience function to create the controls
+layout = [
+    ss.record('Restaurant', 'name'),
+    ss.record('Restaurant', 'location'),
+    ss.record('Restaurant', 'fkType', sg.Combo)]
+sub_layout = [
+    [sg.Listbox(values=(), size=(35, 10), key="SELECTOR.Item", select_mode=sg.LISTBOX_SELECT_MODE_SINGLE, enable_events=True),
+    sg.Col(
+        [ss.record('Item', 'name'),
+         ss.record('Item', 'fkMenu', sg.Combo),
+         ss.record('Item', 'price'),
+         ss.record('Item', 'description', sg.MLine, (30, 7))
+         ])],
+    ss.record_navigation('Item',navigation=False, search=False)
+]
+layout += [[sg.Frame('Items', sub_layout)]]
+layout += [ss.record_navigation('Restaurant',protect=True,search=True,save=True)]
+
+# Initialize our window and database, then bind them together
+win = sg.Window('places to eat', layout, finalize=True)
+db = ss.Database('example2.db', win, sql_file='example2.sql')      # <=== load the database and bind it to the window
+
+# Set our callbacks
+db.set_callback('edit_enable',en)
+db.set_callback('edit_disable',dis)
+
+while True:
+    event, values = win.read()
+    if db.process_events(event, values):                  # <=== let PySimpleSQL process its own events! Simple!
+        print('PySimpleDB event handler handled the event!')
+    elif event == sg.WIN_CLOSED or event == 'Exit':
+        db=None              # <= ensures proper closing of the sqlite database and runs a database optimization
+        break
+    else:
+        print(f'This event ({event}) is not yet handled.')
