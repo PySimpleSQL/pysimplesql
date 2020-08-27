a63ece2e46db098e39987e95cbe3ce911ac480c8
Jon Decker <jondecker76@gmail.com>
2020-08-27 10:46:48 -0400

    Callback example

--- example_callback.py
+++ example_callback.py
@@ -2,11 +2,12 @@
 import PySimpleGUI as sg
 import PySimpleSQL as ss                               # <=== PySimpleSQL lines will be marked like this.  There's only a few!
 
+# Here are our callback functions
 def en(db,win):
-    res=sg.popup_yes_no('Are you sure you want to enable?')
-    return True if res=='Yes' else False
+    res=sg.popup_get_text('Enter password for edit mode.\n(Hint: it is 1234)')
+    return True if res=='1234' else False
 def dis(db,win):
-    res = sg.popup_yes_no('Are you sure you want to DISABLE?')
+    res = sg.popup_yes_no('Are you sure you want to disabled edit mode?')
     return True if res == 'Yes' else False
 
 # Define our layout. We will use the ss.record convenience function to create the controls
@@ -30,8 +31,11 @@ layout += [ss.record_navigation('Restaurant',protect=True,search=True,save=True)
 # Initialize our window and database, then bind them together
 win = sg.Window('places to eat', layout, finalize=True)
 db = ss.Database('example2.db', win, sql_file='example2.sql')      # <=== load the database and bind it to the window
+
+# Set our callbacks
 db.set_callback('edit_enable',en)
 db.set_callback('edit_disable',dis)
+
 while True:
     event, values = win.read()
     if db.process_events(event, values):                  # <=== let PySimpleSQL process its own events! Simple!
