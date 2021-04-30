8f85618ef2b486eb09984967177df39307210945
Jon Decker <jondecker76@gmail.com>
2021-04-30 14:16:32 -0400

    working on some PyPi stuff.  Hopefully I can have it there soon!

--- setup.py
+++ setup.py
@@ -8,18 +8,20 @@ def readme():
     except IOError:
         return ''
 
+requirements = ['logging','PySimpleGUI','sqlite3','functools','os']
 
 setuptools.setup(
     name="pysimplesql",
-    version="0.1.1",
+    version="2021.5.1",
     author="Jonathan Decker",
     author_email="pysimplesql@gmail.com",
     description="sqlite3 database binding for PySimpleGUI",
     long_description=readme(),
     long_description_content_type="text/markdown",
-    keywords="SQL database application front-end access libre office GUI PySimpleGUI",
+    keywords="SQL sqlite database application front-end access libre office GUI PySimpleGUI",
     url="https://github.com/PySimpleSQL/PySimpleSQL",
     packages=setuptools.find_packages(),
+    install_requires=requirements,
     classifiers=(
         "Programming Language :: Python :: 3"
         "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
