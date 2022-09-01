ba2f5a212adb396adc6a2807f3109b79cdac5a2e
Jon Decker <jondecker76@gmail.com>
2022-09-01 19:26:46 -0400

    fixed the settings.py example

--- examples/settings.py
+++ examples/settings.py
@@ -17,6 +17,10 @@ INSERT INTO SETTINGS VALUES (3,'antialiasing', True,'Would you like to render wi
 INSERT INTO SETTINGS VALUES (4, 'query_retries', 3,'Retry queries this many times before aborting.');
 """
 
+frm = ss.Form('Settigs.db', sql_commands=sql)      # <=== load the database
+# Note: we are not binding this Form to a window yet, as the window has not yet been created.
+# Creating the form now will help us get values for the tooltips during layout creation.
+
 # When using Form.record() to create entries based on key/value pairs, it just uses an extended syntax.
 # Where Form.record('Settings.value') would return the value column from the Settings table FOR THE CURRENT RECORD,
 # the extended syntax of Form.record('Settings.value?key=first_name') will return the value column from the Settings
@@ -25,14 +29,14 @@ INSERT INTO SETTINGS VALUES (4, 'query_retries', 3,'Retry queries this many time
 layout=[
     [sg.Text('APPLICATION SETTINGS')],
     [sg.HorizontalSeparator()],
-    ss.record('Settings.value?key=company_name'),
+    ss.record('Settings.value?key=company_name', tooltip = frm['Settings'].get_keyed_value('description', 'key', 'company_name')),
     # Notice how we can use get_keyed_value() to retrieve values from keys in the query.  We are using it to set tooltips.
     [sg.Text('')],
-    ss.record('Settings.value?key=debug_mode',sg.CBox),
+    ss.record('Settings.value?key=debug_mode',sg.CBox, tooltip=frm['Settings'].get_keyed_value('description', 'key', 'debug_mode')),
     [sg.Text('')],
-    ss.record('Settings.value?key=antialiasing', sg.CBox),
+    ss.record('Settings.value?key=antialiasing', sg.CBox, tooltip=frm['Settings'].get_keyed_value('description', 'key', 'antialiasing')),
     [sg.Text('')],
-    ss.record('Settings.value?key=query_retries'),
+    ss.record('Settings.value?key=query_retries', tooltip=frm['Settings'].get_keyed_value('description', 'key', 'query_retries')),
     # For the actions, we don't want to offer users to insert or delete records from the settings table,
     # and there is no use for navigation buttons due to the key,value nature of the data.  Therefore, we will
     # disable all actions (default=False) except for the Save action (save=True)
@@ -41,12 +45,7 @@ layout=[
 
 # Initialize our window then bind it to the Form
 win = sg.Window('Preferences: Application Settings', layout, finalize=True)
-frm = ss.Form('Settigs.db', sql_commands=sql, bind=win)      # <=== load the database and bind it to the window
-
-win['Settings.value?key=company_name'].update(tooltip = frm['Settings'].get_keyed_value('description', 'key', 'company_name'))
-win['Settings.value?key=debug mode'].update(tooltip=frm['Settings'].get_keyed_value('description', 'key', 'debug_mode'))
-win['Settings.value?key=antialiasing'].update(tooltip=ss['Settings'].get_keyed_value('description', 'key', 'antialiasing'))
-win['Settings.value?key=query_retries'].update(tooltip=frm['Settings'].get_keyed_value('description', 'key', 'query_retries'))
+frm.bind(win)
 
 print(frm['Settings'].get_keyed_value('description', 'key', 'debug_mode'))
 
@@ -68,6 +67,7 @@ storing settings for your own program
 Learnings from this example:
 - embedding sql commands in code for table creation
 - creating a default/empty database with sql commands with the sql_commands keyword argument to ss.Form()
+- Creating a form without binding to a window, then later binding the form to a window with a separate statement
 - using ss.record() and ss.actions() functions for easy GUI element creation
 - using the extended key naming syntax for keyed records (Query.value_column?key_column=key_value)
 - using the Query.get_keyed_value() method for keyed data retrieval
