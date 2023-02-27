058f726cf5444920a3374b15f4af85af0acc1c3e
ssweber <57631333+ssweber@users.noreply.github.com>
2023-02-26 19:11:11 -0500

    only put key, not entire table instance in cache

--- pysimplesql/pysimplesql.py
+++ pysimplesql/pysimplesql.py
@@ -2165,11 +2165,11 @@ class Form:
                         except ValueError:
                             index = []
                         logger.debug(f'Selector:: index:{index} found:{found}')
-                        if element in table.element_cache.keys():
-                            if table.element_cache[element] != [values,index]:
+                        if element.key in table.element_cache.keys():
+                            if table.element_cache[element.key] != [values,index]:
                                 element.update(values=values,select_rows=index)
                         else: element.update(values=values,select_rows=index) # first time this selector won't be in the cache                          
-                        table.element_cache[element] = [values,index] # fastest when here and not under element.update()
+                        table.element_cache[element.key] = [values,index] # update cache
                         # set vertical scroll bar to follow selected element
                         if len(index): element.set_vscroll_position(pk_position)
                         eat_events(self.window)
