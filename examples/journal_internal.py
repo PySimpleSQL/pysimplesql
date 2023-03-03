54272aee538fc2cd1229f14f225c74b572f9a234
ssweber <57631333+ssweber@users.noreply.github.com>
2023-03-03 11:00:30 -0500

    Example Harmony
    One final idea. I got this after you used Table.Column for the key in record().
    
    Why not just use same concept for selector/action?
    
    And (borrowing from keygen), allow Unique identifier for all 3 by adding a ':' after the table, or table.column name?

--- examples/journal_internal.py
+++ examples/journal_internal.py
@@ -47,14 +47,14 @@ INSERT INTO Journal (id, mood_id, title, entry) VALUES (12, 4, 'I Found the Solu
 # -------------------------
 # Define the columns for the table selector using the TableHeading convenience class.  This will also allow sorting!
 headings=ss.TableHeadings(sort_enable=True)
-headings.add_column('Title', 'title', width=40)
-headings.add_column('Date',  'entry_date', width=10)
-headings.add_column('Mood',  'mood_id', width=20)
+headings.add_column('title', 'Title',  width=40) # column_name, heading, 
+headings.add_column('entry_date', 'Date', width=10)
+headings.add_column('mood_id', 'Mood', width=20)
 
 layout=[
-    [ss.selector('sel_journal','Journal',sg.Table,num_rows=10,headings=headings)],
-    [ss.actions('act_journal','Journal',edit_protect=False)],
-    [ss.record('Journal.entry_date')],
+    [ss.selector('Journal:sel',sg.Table,num_rows=10,headings=headings)],
+    [ss.actions('Journal:act',edit_protect=False)],
+    [ss.record('Journal.entry_date:MyUniqueKey')],
     [ss.record('Journal.mood_id', sg.Combo, label='My mood:', size=(30,10), auto_size_text=False)],
     [ss.record('Journal.title')],
     [ss.record('Journal.entry', sg.MLine, size=(71,20))]
