943a7537a53805a7fc804d71e4c10800d2b2e9c7
ssweber <57631333+ssweber@users.noreply.github.com>
2023-03-10 14:32:08 -0500

    FIX FIX FIX ResultSet
    I copy pasted your lost changes... incorrectly.

--- pysimplesql/pysimplesql.py
+++ pysimplesql/pysimplesql.py
@@ -4157,11 +4157,11 @@ class ResultSet:
 
     def __setitem__(self, idx:int, new_row:ResultRow):
         # carry over the original_index
-        new_row.original_index = self.rows[idx].original_index
         try:
             new_row.original_index = self.rows[idx].original_index
         except AttributeError:
             pass
+        self.rows[idx]=new_row
 
 
     def __len__(self):
