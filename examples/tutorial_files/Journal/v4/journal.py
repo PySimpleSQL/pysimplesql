ede4e6c13db19d3fab46501b5da4a4f52652880c
ssweber <57631333+ssweber@users.noreply.github.com>
2023-03-26 13:46:22 -0400

    Turn off formatting in examples for now

--- examples/tutorial_files/Journal/v4/journal.py
+++ examples/tutorial_files/Journal/v4/journal.py
@@ -1,3 +1,6 @@
+# To keep examples concise, avoid Black formatting. Remove # fmt: off to use Black formatting.
+# fmt: off
+
 # import PySimpleGUI and pysimplesql
 import PySimpleGUI as sg
 import pysimplesql as ss
